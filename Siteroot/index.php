b3a87a892e6c8f7eb3eece957b2e16f8694229bf
Rene <GitHub@nocommerce.nl>
2012-08-05 21:04:29 +0200

    Silex startup

--- Siteroot/index.php
+++ Siteroot/index.php
@@ -5,13 +5,30 @@ define( "ROOTPATH", SITEROOTPATH . '/../' );
 define( "SILEXROOTPATH", ROOTPATH . '/silex/vendor/' );
 define( "TWIGROOTPATH", ROOTPATH . '/twig/lib/' );
 
+function printr( $what, $text = 'Output', $halt = false){
+  echo"<pre>" . $text . " " . print_r( $what, 1 ) . "</pre>";
+  if( $halt ){
+    exit;
+  }
+}
+
 //to show my own solution,...
 if( isset( $_GET['org'] ) ){
   require( 'altindex.php' );
   exit();
 }
 else{
-  echo "Silex and twig,...";
+  require_once SILEXROOTPATH.'autoload.php';
+
+  $app = new Silex\Application();
+
+  $app->get('/hello/{name}', function ($name) use ($app) {
+    printr( $name );
+      return 'Hello '.$app->escape($name);
+  });
+
+  $app->run();
+  
 }
 
 ?>
