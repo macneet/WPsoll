06c20f768f42b830c84e7deef410a191b933bc3e
Rene <GitHub@nocommerce.nl>
2012-08-08 17:54:37 +0200

    Silex calculon app

--- Siteroot/app/index.php
+++ Siteroot/app/index.php
@@ -1,32 +1,43 @@
 <?php
 
-define( "SITEROOTPATH", dirname( __FILE__ ) );
-define( "ROOTPATH", SITEROOTPATH . '/../../' );
+define( "SITEROOTPATH", dirname( __FILE__ ) . '/' );
+define( "ROOTPATH", SITEROOTPATH . '../../' );
 define( "SILEXROOTPATH", ROOTPATH . 'silex/vendor/' );
 define( "TWIGTEMPLATESPATH", ROOTPATH . 'templates/' );
 
-function printr( $what, $text = 'Output', $halt = false){
-  echo"<pre>" . $text . " " . print_r( $what, 1 ) . "</pre>";
-  if( $halt ){
-    exit;
-  }
-}
-
 require_once SILEXROOTPATH.'autoload.php';
-$app = new Silex\Application();
-$app['debug'] = ( getenv('APP_DEV_STATUS') === 'development' );
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
+
+$myCalculon = new Silex\Application();
+$myCalculon['debug'] = ( getenv('APP_DEV_STATUS') === 'development' );
+$myCalculon->register( new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => TWIGTEMPLATESPATH,
 ));
 
-$app['calculator'] = function () {
-    return new Service();
-};
 
-$app->get('/calculon/', function () use ($app) {
-    return $app['twig']->render('calculon.twig' );
+$myCalculon->match( 'calculon/result' , function () use ( $myCalculon ) {
+    require_once( SITEROOTPATH . "calculator.php" );
+  
+    $myCalculon->register( new Silex\Provider\SessionServiceProvider( array( 'name' => 'calculon' ) ) );
+
+    $myCalculon['data'] = $myCalculon->before(function ( Request $request ) {
+        if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+            $data = json_decode($request->getContent(), true);
+            $request->request->replace(is_array($data) ? $data : array());
+        }
+    });
+    
+    
+    $myCalculon['calculator_service'] = function( $myCalculon ){
+      return new calculator( $myCalculon );
+    };
+
+    return $myCalculon->json( $myCalculon['calculator_service'] );
 });
 
-$app->run();
 
+$myCalculon->get('/', function () use ($myCalculon) {
+    return $myCalculon['twig']->render('calculon.twig' );
+});
+
+$myCalculon->run();
 ?>
